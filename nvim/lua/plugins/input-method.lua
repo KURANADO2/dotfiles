eef536e52eaa98c9728b2177b6d80a80c58b7d23
Xinling Jing <jingxinling1024@163.com>
2024-12-12 10:40:55 +0800

    fix: Fixed im-select unavailable issue

--- nvim/lua/plugins/input-method.lua
+++ nvim/lua/plugins/input-method.lua
@@ -1,6 +1,8 @@
 return {
-  -- "keaising/im-select.nvim",
-  -- config = function()
-  --   require("im_select").setup({})
-  -- end,
+  "keaising/im-select.nvim",
+  config = function()
+    require("im_select").setup({
+      default_command = "/opt/homebrew/bin/im-select",
+    })
+  end,
 }
