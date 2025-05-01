71bc81ad2249e65e63337ccc88875472be6cd59c
Xinling Jing <jingxinling1024@163.com>
2025-05-01 19:50:40 +0800

    fix: Fix im-select missing in ubuntu

--- nvim/lua/plugins/input-method.lua
+++ nvim/lua/plugins/input-method.lua
@@ -1,8 +1,12 @@
-return {
-  "keaising/im-select.nvim",
-  config = function()
-    require("im_select").setup({
-      default_command = "/opt/homebrew/bin/im-select",
-    })
-  end,
-}
+if vim.fn.has("mac") == 1 then
+  return {
+    "keaising/im-select.nvim",
+    config = function()
+      require("im_select").setup({
+        default_command = "/opt/homebrew/bin/im-select",
+      })
+    end,
+  }
+else
+  return {}
+end
