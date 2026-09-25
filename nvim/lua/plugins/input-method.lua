c45d8e03984cf98a509fabbd37a96b02264b0302
Xinling Jing <jingxinling1024@163.com>
2026-09-25 22:01:37 +0800

    fix: use macism replace im-select

--- nvim/lua/plugins/input-method.lua
+++ nvim/lua/plugins/input-method.lua
@@ -2,9 +2,8 @@ if vim.fn.has("mac") == 1 then
   return {
     "keaising/im-select.nvim",
     config = function()
-      require("im_select").setup({
-        default_command = "/opt/homebrew/bin/im-select",
-      })
+      -- 插件在 macOS 上默认使用 macism(arm64 原生),旧的 im-select 是 x86_64 已无法运行
+      require("im_select").setup({})
     end,
   }
 else
